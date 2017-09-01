0b23bd85b25985a63c9301b794d7e4d57117644d
tungntd00605 <tungntd00605@fpt.edu.vn>
2017-09-01 20:11:39 +0700

    Homework session 2 from 31/8/2017

--- 123.c
+++ 123.c
@@ -3,31 +3,52 @@
 //
 
 #include <stdio.h>
+#include <ctype.h>
+
+// tính tổng a + b
 int plus(int a, int b){
     return a + b;
 }
+// tính hiệu a - b
 int minus(int a, int b){
     return a - b;
 }
+// tính tích a*b
 int multi(int a, int b){
     return a * b;
 }
-
-int divide(int a, int b){
+// chuyển a về dạng số thực (float) tính thương a/b và kiểm tra nếu b=0 thì nhập lại số b
+float division(int a, int b){
+    float c;
+    c = a;
     if(b == 0){
         printf("The divisor cannot be zero, please input another number \n");
         scanf("%d", &b);
     }
-        return a / b;
+    return c / b;
 }
+// nhập số a
+int inputa(){
+    int a;
+    printf("Enter number a: ");
+    scanf("%d", &a);
+    return a;
+}
+// nhập số b
+int inputb(){
+    int b;
+    printf("Enter number b: ");
+    scanf("%d", &b);
+    return b;
+}
+
 main() {
     int a, b;
-    printf("Nhap vao so a: ");
-    scanf("%d", &a);
-    printf("Nhap vao so b: ");
-    scanf("%d", &b);
-    printf("Tong hai so: %d\n", plus(a, b));
-    printf("Hieu hai so: %d\n", minus(a, b));
-    printf("Tich hai so: %d\n", multi(a, b));
-    printf("Thuong hai so: %d\n", divide(a, b));
+    a = inputa();
+    b = inputb();
+    printf("Sum of total: %d\n", plus(a, b));
+    printf("Difference: %d\n", minus(a, b));
+    printf("Product: %d\n", multi(a, b));
+    printf("Quotient: %f\n", division(a, b));
 }
+// còn phần xét nếu a,b có phải số hay là chuỗi nhưng chưa tìm được cách hợp lý để sử dụng với kiến thức từ session 6 trờ xuống
